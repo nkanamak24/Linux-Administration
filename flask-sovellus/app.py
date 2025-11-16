5a10fd65cf3771d65f8b57b1222f132e0cc63302
nkanamak24 <nkanamak24@students.oamk.fi>
2025-11-16 19:35:20 +0000

    Poistettu vanha Flask-sovellus

--- flask-sovellus/app.py
+++ flask-sovellus/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 import mysql.connector
 from datetime import datetime, timedelta
@@ -29,30 +30,53 @@ def home():
     <!DOCTYPE html>
     <html>
     <head>
-       
-<meta charset="UTF-8">
-<title>Samaa vanhaa</title>
-<style>
-    body {{
-        background-color: #FFFF00;
-        font-family: Arial, sans-serif;
-        color: black;
-        padding: 20px;
-    }}
-    h1 {{
-        color: #8A2BE2;
-    }}
-    p {{
-        font-size: 18px
-    }}
+        <meta charset="UTF-8">
+        <title>Samaa vanhaa</title>
+        <style>
+            body {{
+                background-color: #FFFF00;
+                font-family: Arial, sans-serif;
+                color: black;
+                padding: 20px;
+            }}
+            h1 {{
+                color: #8A2BE2;
+            }}
+            p {{
+                font-size: 18px;
+            }}
+            button {{
+                background-color: #1E90FF;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                font-size: 18px;
+                border-radius: 5px;
+                cursor: pointer;
+            }}
+            button:hover {{
+                background-color: #4682B4;
+                }}
         </style>
     </head>
     <body>
         <h1>Linux Administration, Tervetuloa ensimmäiselle sivulleni</h1>
         <p>Kellonaika tietokannasta (Suomen aika): {muotoiltu_aika}</p>
+        <h3>Siirry Data-analyysiin</h3>
+        <button onclick="siirryDataAnalyysiin()">Klikkaa tästä!</button>
+
+        <script>
+            function siirryDataAnalyysiin() {{
+                window.location.href = 'http://195.148.22.146/data-analysis';
+            }}
+        </script>
     </body>
     </html>
     """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
+
+
+
